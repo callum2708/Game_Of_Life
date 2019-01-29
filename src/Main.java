ce7c46bc9a5710dfffbe762582efa583732a3434
Callum Perks <ll15clp@leeds.ac.uk>
2019-01-29 17:23:02 +0000

    Added class files to gitignore and made size of the grid be a command line argument

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args)  throws InterruptedException{
-        Simulation simulation = new Simulation(10, 10);
+        Simulation simulation = new Simulation(Integer.parseInt(args[1]), Integer.parseInt(args[1]));
         simulation.start();
     }
 }
